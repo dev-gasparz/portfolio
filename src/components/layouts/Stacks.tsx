3012e36e6787964538e1596f20737689cf12fb7b
Gaspar <dev_gaspar@outlook.com>
2025-04-21 19:42:23 -0300

    feat: criado json de links para as stacks e criado componente para o mesmo

--- src/components/layouts/Stacks.tsx
+++ src/components/layouts/Stacks.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import stacks from "../../assets/images/stacks-badges.json";
+import comet from "../../assets/images/comet.png";
+
+type stackBadges = {
+  title: string;
+  url: string;
+};
 
 const Stacks: React.FC = () => {
   return (
-    <div>
-      <h2>My Stacks</h2>
-    </div>
+    <section className="stacks container">
+      <div className="stacks-content">
+        <div className="stacks-left-content">
+          <h2>Minhas stacks</h2>
+          <div className="tech-badges">
+            {stacks.map((badge: stackBadges) => (
+              <img
+                key={badge.title}
+                src={badge.url}
+                alt={badge.title}
+                title={badge.title}
+              />
+            ))}
+          </div>
+        </div>
+        <img className="comet" src={comet} alt="" />
+      </div>
+    </section>
   );
 };
 
