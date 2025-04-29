b08f80906848ab58c2a4b640d0ee2e6dc2aadbc6
Gaspar <dev_gaspar@outlook.com>
2025-04-29 18:25:24 -0300

    feat: adicionado dashboard fintech a lista de projetos

--- src/components/layouts/Projects/ProjectWrap.tsx
+++ src/components/layouts/Projects/ProjectWrap.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Projects from ".";
 import projetoPortf from "../../../assets/images/projeto-portfolio.png";
+import projetoFintech from "../../../assets/images/fintech.png";
 
 const ProjectWrap: React.FC = () => {
   return (
@@ -14,7 +15,16 @@ const ProjectWrap: React.FC = () => {
           gitHref="https://github.com/dev-gasparz/portfolio"
           projectHref="https://portfolio-tau-kohl-37.vercel.app/"
           position="toLeft"
-          stack={["React.js", "TypeScript", "SASS"]}
+          stack={["React", "TypeScript", "SASS"]}
+        />
+        <Projects
+          imgUrl={projetoFintech}
+          title="Dashboard Fintech"
+          text="Aplicação construída com React, TypeScript e foco no uso prático da tipagem estática no desenvolvimento de interfaces modernas. O projeto simula um painel financeiro, exibindo dados como vendas, recebimentos e transações processadas, com gráficos e filtros dinâmicos. Durante o desenvolvimento, foram aplicadas boas práticas com hooks tipados, organização de componentes e manipulação segura de dados."
+          gitHref="https://github.com/dev-gasparz/fintech"
+          projectHref="https://fintech-phi-six.vercel.app/"
+          position="toRight"
+          stack={["React", "TypeScript", "Vite", "Recharts"]}
         />
       </div>
     </section>
